c0b3655415ce8d5e8dbaa0c644c9ef35ada3c495
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-26 00:36:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,5 @@
 def line(kd)
   queue = "The line is currently:"
-  kd.each_index { |position| queue << " #{position + 1}. #{kd[postion]}"}
+  kd.each_index { |position| queue << " #{position + 1}. #{kd[position]}"}
   puts kd.empty? ? "The line is currently empty." : queue
 end
