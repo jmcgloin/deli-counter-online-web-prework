19236de4bb4dad409a8b7506bb500843d7a5f9f2
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-26 00:21:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,5 @@
 def line(line)
-  if line.empty
+  if line.empty?
     puts "The line is currently empty."
   else
     line.each_index { |position| puts "The line is currently: #{position}. #{line[position]}" }
