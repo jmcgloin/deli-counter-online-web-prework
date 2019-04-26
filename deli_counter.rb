14bf7f36c985d0d2f43e514c0fddb2ebd9c8b867
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-26 00:27:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
-def line(line)
-  if line.empty?
+def line(katz_deli)
+  if katz_deli.empty?
     puts "The line is currently empty."
   else
     queue = "The line is currently:"
-    line.each_index { |position| queue + " #{position + 1}. #{line[position]}" }
+    katz_deli.each_index { |position| queue + " #{position + 1}. #{katz_deli[position]}" }
   end
   puts queue
 end
